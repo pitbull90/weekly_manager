9ec3c549d6ba3830f959149d6cdb05e9aaaffd68
pitbull90 <henrycordero@live.com>
2024-09-14 10:13:39 -0700

    created function to add task to respective day

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-function addTask(dayId, inputId) {
+function addTask(event, dayId, inputId) {
+  event.preventDefault(); // Prevent form submission from reloading the page
+
   const taskInput = document.getElementById(inputId);
   const taskList = document.getElementById(
     `taskList${capitalizeFirstLetter(dayId)}`
@@ -8,5 +10,18 @@ function addTask(dayId, inputId) {
   if (taskValue) {
     const taskItem = document.createElement('div');
     taskItem.setAttribute('class', 'task');
+
+    const task = document.createElement('label');
+    task.setAttribute('class', 'task-label');
+    task.textContent = taskValue; // Set the task value as the label text
+
+    taskItem.appendChild(task);
+    taskList.appendChild(taskItem);
+
+    taskInput.value = ''; // Clear input field after adding task
   }
+}
+
+function capitalizeFirstLetter(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
 }
